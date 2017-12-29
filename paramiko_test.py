e5260d7d3feb7abcb8062c25fbf2065205dd2774
Kári Harðarson <kari@tern.is>
2017-12-29 18:10:59 +0000

    add test for xml edit

--- paramiko_test.py
+++ paramiko_test.py
@@ -1,17 +1,32 @@
 import paramiko
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('10.10.10.84', username='tern', password='ternuser')
-i,o,e=ssh.exec_command('ls -l')
-
-print "output"
-print o.read()
-
-print "error"
-print e.read()
-
 from scp import SCPClient
 
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('10.10.10.171', username='tern', password='ternuser')
+
+if False:
+    i,o,e=ssh.exec_command('ls -l')
+
+    print "output"
+    print o.read()
+
+    print "error"
+    print e.read()
+
 with SCPClient(ssh.get_transport()) as scp:
-    scp.put('test.txt', 'test2.txt')
-    scp.get('test2.txt')
+    scp.get('test.xml',"foo.xml")
+    import xml.etree.ElementTree as ET
+    tree = ET.parse('foo.xml')
+    rootnode=tree.getroot()
+    tree.findall("./cplCorrelationReceiveMethod[@method]")[0].set("kari","gogo")
+    for path in     ["./cplTrackUdpReceiverRdps/[@address]",
+                    "./correlationUdpReceiverRdps/[@address]",
+                    "./cplTrackUdpReceiverArtas/[@address]",
+                    "./correlationUdpReceiverArtas/[@address]",
+                    "./edgeSimulatorClockReceiver/[@address]",
+                    "./flightInfoSource/[@method]"]:
+        tree.findall(path)[0].set("kari","fofo")              
+
+    tree.write('foo2.xml')
+    scp.put('foo.xml', 'test.xml')
